f024dec6d246cdf3dc5efe79885c355ea458b635
Bhargavi Borra <82596888+Bhargavi506@users.noreply.github.com>
2023-09-07 10:08:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Webhook integration" }');
+    res.send('{ "response": "Webhook integration with jenkins is working fine" }');
 });
 app.get('/ready', function (req, res) {
     res.send('{ "response": " Great!, It works!" }');
