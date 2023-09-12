a0099c06e5016e973f3794154d620c27d74a2cd1
Bhargavi Borra <82596888+Bhargavi506@users.noreply.github.com>
2023-09-12 15:37:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Deploy to kubernetes" }');
+    res.send('{ "response": "Deploy to kubernetes." }');
 });
 app.get('/ready', function (req, res) {
     res.send('{ "response": " Great!, It works!" }');
